18a29ac6702b14f0f3d54465ff9d7b067e54a182
itsuki-chan <82200440+Adiixyz@users.noreply.github.com>
2021-09-09 11:00:05 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const { Telegraf } = require('telegraf');
+const fs = require('fs')
+const config = JSON.parse(fs.readFileSync('./configbot.json'))
+const bot = new Telegraf(config.bot-token);
+const translate = require('@vitalets/google-translate-api')
+// start
+bot.start(ctx => ctx.reply('Welcome, Silahkan Gunakan Bot Translate Ke Bahasa English Dengan Baik:)'));
+// message 
+bot.on('message', ctx => {
+const lang = 'en'
+translate(`${ctx.message.text}`,{to:`${lang}`}).then( res => {
+     const translation = res.text
+      ctx.reply(translation);
+    });
+});
+//started
+bot.startPolling();
+/*
+* Simple Bot By Adiixyz :)
+*/
